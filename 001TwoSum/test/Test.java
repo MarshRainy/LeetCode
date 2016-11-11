1c2a0792c755febee32f4cbb6639092002c8fb1f
MarshRainy <nahuy2177@gmail.com>
2016-11-11 17:33:36 +0800

    [更新]001TwoSum, 格式化等
    格式化了代码

--- 001TwoSum/test/Test.java
+++ 001TwoSum/test/Test.java
@@ -1,13 +1,12 @@
 import p001.Solution;
 
 public class Test {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int [] nums = new int[]{2,7,1,34};
-        int [] result = solution.twoSum(nums,9);
-        for (int i :
-                result) {
-            System.out.println(i);
-        }
-    }
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] nums = new int[]{2, 7, 1, 34};
+		int[] result = solution.twoSum(nums, 9);
+		for (int i : result) {
+			System.out.println(i);
+		}
+	}
 }
